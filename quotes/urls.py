04d42e97c850790929a31d8273175479982632c6
Ayeni Oluwaseun <78281232+Oluwaseun-ayeni@users.noreply.github.com>
2022-10-11 19:55:51 +0100

    updated

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,17 +4,13 @@ from django.urls import path,include
 from quotes.views import *
 
 
-urlpatterns = [
-    path('api/generate_quote/', views.GenerateQuote.as_view()),
-    # path('api/display_quote/', views.DisplayQuote.as_view()),
-    
-]
 
 router = SimpleRouter()
 router.register('quote', views.QuoteViewSet,basename="quote")
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api/generate_quote/', views.GenerateQuote.as_view()),
 
     
 ]
